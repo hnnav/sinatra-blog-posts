c1e94fc4ec00d5507d678a2e8defbf2a01137017
hnnav <henna.v@icloud.com>
2020-04-11 15:23:01 +0300

    updated user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,30 +3,16 @@ require "./app/models/user"
 
 class UserController < ApplicationController
 
-    get '/users/signup' do
-        erb :'/users/signup'
-    end
-
-    get '/users/login' do
-        erb :'/users/login'
-    end
-
-    post '/users/signup' do
-        if params[:password] == params[:password_confirmation]
-            @user = User.create(
-             username: params[:username], 
-             password: params[:password]
-            )
-            session[:user_id] = @user.id 
-            redirect "/users/#{@user.id}"
+    # render login form
+    get '/users/login' do 
+        if !session[:user_id]
+          erb :'users/login'
+        else
+          redirect "/users/#{@current_user.id}"
         end
     end
 
-    get '/users/:id' do
-        @user = User.find(params[:id]) 
-        erb :'users/show'
-    end
-
+    # set user and session
     post '/users/login' do 
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
@@ -37,4 +23,38 @@ class UserController < ApplicationController
         end 
     end 
 
+    # render signup form
+    get '/users/signup' do
+        if !session[:user_id]
+            erb :'/users/signup'
+        else
+            redirect "/users/#{@current_user.id}"
+        end
+    end
+
+    # create user instance, set session
+    post '/users/signup' do
+        if params[:username] == "" || params[:password] == ""
+            redirect to '/users/signup'
+        elsif params[:password] == params[:password_confirmation]
+            @user = User.create(
+             username: params[:username], 
+             password: params[:password]
+            )
+            session[:user_id] = @user.id 
+            redirect "/users/#{@user.id}"
+        else
+            "passwords don't match"
+        end
+    end
+
+    # logout method
+    get '/logout' do
+        if session[:user_id] != nil
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+    end
 end
